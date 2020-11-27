6a3cf80bc952120af0d6a26f0d45f5d4f3dd2d06
Fernando Bustamante <fernando.miguel.bustamante@gmail.com>
2020-11-26 22:21:48 -0300

    endpoint compra hard - volver version 0.16

--- src/controllers/producto.js
+++ src/controllers/producto.js
@@ -176,6 +176,72 @@ export const operacionCompra = async (req, res) => {
     console.log("Cambio a base de datos: '" + nombre_de_base_de_datos + "'.") 
   });
  
+  //const id_comprador = await f.decodeId(req.headers);
+
+  const id_comprador = 125;
+  const importe = 1000;
+  const id_vendedor = 114;
+  const id_producto = 16;
+
+  //------ DATOS DE LA COMPRA
+  var post_usuario = [ importe, id_comprador, id_vendedor, 1 ];
+  var query1 = "INSERT INTO operaciones (fecha, importe, id_comprador, id_vendedor, id_pago ) \
+  VALUES (NOW(), ?, ?, ?, ?);";
+
+  console.log({ query: query1, variables: { post_usuario: post_usuario } });
+  const resultado1 = await query(query1, post_usuario);
+
+  console.log(resultado1);
+
+  //------- DATOS DEL VENDEDOR
+  const query2 = "SELECT * FROM usuarios WHERE id_usuario = ?;";
+  console.log({ query: query2, variables: { id_vendedor: id_vendedor } });
+  const resultado2 = await query(query2, id_vendedor);
+
+  console.log(resultado2)
+
+  //------- DATOS DEL PRODUCTO
+  var query3 = "SELECT * FROM productos WHERE id_producto = ?;";
+  console.log({ query: query3, variables: { id_producto: id_producto } });
+  const resultado3 = await query(query3, id_producto);
+
+  console.log(resultado3)
+
+  const texto = "Vendiste: " + resultado3[0].nombre + ". Por: ARS " + resultado3[0].precio + ". \
+  Al usuario: " + resultado2[0].nombre + " " + resultado2[0].apellido + ". Contactalo para coordinar la entrega. \
+  Su teléfono es: " + resultado2[0].telefono + ". Su correo es: " + resultado2[0].email + ". Éxitos!!! Seguí vendiendo y ganando dinero.";
+
+  console.log(texto)
+
+  f.enviar_correo("Whales", resultado2[0].email, "Felicitaciones! vendiste " + resultado3[0].nombre, texto);
+
+  const msg = {estado: 201, info_compra: resultado1, data_comprador: resultado2, mensage: "Compra exitosa, correo enviado."}
+
+  console.log(msg);
+
+  res.status(201).json(msg);
+
+}
+
+export const operacionCompraOriginal = async (req, res) => {
+  const util = require('util');
+  const db = require('mysql');
+  const host = process.env.MYSQL_HOST;
+  const nombre_de_base_de_datos = 'trabajo_final01';
+  const con = db.createConnection(config);
+  // node native promisify
+  const query = util.promisify(con.query).bind(con);
+
+  con.connect(err => {
+      if (err) throw err;
+      console.log("Conectado a la base de datos " + host + ".");
+  });
+
+  con.changeUser({database: nombre_de_base_de_datos}, err => {
+    if (err) throw err;
+    console.log("Cambio a base de datos: '" + nombre_de_base_de_datos + "'.") 
+  });
+ 
   const id_comprador = await f.decodeId(req.headers);
 
   //------ DATOS DE LA COMPRA
@@ -216,23 +282,4 @@ export const operacionCompra = async (req, res) => {
 
   res.status(201).json(msg);
 
-}
-
-
-export const operacionCompraOriginal = async (req, res) => {
-  f.conectar_a_mysql();
-  f.conectar_a_base_de_datos('trabajo_final01');
-  const id_comprador = await f.decodeId(req.headers);
-  //console.log(id_comprador);
-  var post_usuario = [ req.body.importe, id_comprador, req.body.id_vendedor, 1 ];
-  var query = "INSERT INTO operaciones (fecha, importe, id_comprador, id_vendedor, id_pago ) \
-  VALUES (NOW(), ?, ?, ?, ?);";
-  console.log("QUERY: [ " + query + " ], VARIABLES: [ " + post_usuario + " ]");
-  f.query_a_base_de_datos(query, post_usuario)
-    .then(resultado => {
-      var msg = {id_operacion: resultado.insertId, id_comprador: id_comprador, id_vendedor: id_vendedor, mensaje: "Compra procesada."}
-      console.log(msg);
-      res.status(201).json(msg);
-      }, err => console.log(err));
-  f.desconectar_db();
 }
